63b730316f33248b8d2896aa1e52fe3d97a33874
Ravi Kumar <ravkumar@akamai.com>
2017-08-27 13:45:43 -0700

    removed row label and display 12-12

--- calendar-tab/resources/js/google-chart-script.js
+++ calendar-tab/resources/js/google-chart-script.js
@@ -2,6 +2,17 @@ function drawTimeline(events) {
     var container = document.getElementById('calendar');
     var chart = new google.visualization.Timeline(container);
     var dataTable = new google.visualization.DataTable();
+    var startDate= new Date();
+    startDate.setHours(0)
+    startDate.setMinutes(0)
+    startDate.setSeconds(0)
+    startDate.setMilliseconds(0)
+    var endDate = new Date();
+    endDate.setHours(24)
+    endDate.setMinutes(0)
+    endDate.setSeconds(0)
+    endDate.setMilliseconds(0)
+
 
     dataTable.addColumn({
       type: 'string',
@@ -28,9 +39,14 @@ function drawTimeline(events) {
     var options = {
       timeline: {
         groupByRowLabel: true,
+        showRowLabels : false,
         showBarLabels: false,
       },
-      avoidOverlappingGridLines: false
+      avoidOverlappingGridLines: true,
+      hAxis: {
+        minValue: startDate,
+        maxValue: endDate
+      }
     };
 
     chart.draw(dataTable, options);
